5060fb92c04c090027a5e75729d6970c75a78c58
ad <adouani_hatem@yahoo.com>
2026-08-06 22:23:19 +0100

    SRS

---
+++ 01-ring-buffer/inc/ring_buffer.h
@@ -0,0 +1,12 @@
+/**
+ * @file    ring_buffer.h
+ * @brief   Lock-free SPSC circular buffer.
+ * @spec    SRS-RINGBUF-001 Rev A
+ */
+
+ #ifndef RING_BUFFER_H
+ #define RING_BUFFER_H
+
+
+
+ #endif /* RING_BUFFER_H */
